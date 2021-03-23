148f3ffb6fea5af59fb32693b4c4b6fa261d503d
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2021-03-23 19:07:19 +0100

    Add Qt project

---
+++ Qt/Sudoku/mainwindow.cpp
@@ -0,0 +1,220 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QLineEdit>
+#include <QIntValidator>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent),
+        ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+
+    setFixedSize(700, 700); // Dimensioni finestra fisse
+
+    // Blocco l'inserimento di caratteri non numerici
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            QWidget *w = ui->gridLayout->itemAtPosition(i, j)->widget();
+            QLineEdit* cell = static_cast<QLineEdit*>(w);
+            cell->setValidator(new QIntValidator(1, 9));
+        }
+    }
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+void MainWindow::on_pushButton_resolve_clicked()
+{
+    // Disabilito il bottone risolvi
+    ui->pushButton_resolve->setEnabled(false);
+
+    // Setto le celle in sola lettura
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            QWidget *w = ui->gridLayout->itemAtPosition(i, j)->widget();
+            QLineEdit* cell = static_cast<QLineEdit*>(w);
+            cell->setReadOnly(true);
+        }
+    }
+
+    save_sudoku();
+
+    //Controllo se il sudoku in input è valido
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            if (sudoku[i][j] != 0 && !valid_cell(i, j)) {
+                QMessageBox::warning(this, tr("Errore"),
+                                     tr("Sudoku inserito non valido."));
+                return;
+            }
+        }
+    }
+
+    if (solve()) {
+        print_sudoku();
+    }
+    else{
+        QMessageBox::warning(this, tr("Errore"),
+                             tr("Impossibile risolvere il Sudoku."));
+    }
+
+    //Abilito la navigazione tra le mosse
+    current_move = moves.cbegin();
+    if(current_move != moves.cend()) {
+        ui->pushButton_previous->setEnabled(true);
+    }
+}
+
+void MainWindow::on_pushButton_clear_clicked() {
+    // Svuoto pila mosse
+    current_move = nullptr;
+    moves.clear();
+
+    // Disabilito navigazione mosse
+    ui->pushButton_previous->setEnabled(false);
+    ui->pushButton_next->setEnabled(false);
+
+    // Pulisco griglia e riabilito inserimento
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            QWidget *w = ui->gridLayout->itemAtPosition(i, j)->widget();
+            QLineEdit* cell = static_cast<QLineEdit*>(w);
+            cell->clear();
+            cell->setStyleSheet("color: balck");
+            cell->setReadOnly(false);
+        }
+    }
+
+    // Abilito bottone risolvi
+    ui->pushButton_resolve->setEnabled(true);
+}
+
+void MainWindow::on_pushButton_previous_clicked()
+{
+    //Rimuovo mossa più recente
+    QWidget *w = ui->gridLayout->itemAtPosition(
+                current_move->row,
+                current_move->column
+            )->widget();
+    QLineEdit* cell = static_cast<QLineEdit*>(w);
+    cell->setText("");
+
+    ++current_move;
+    //Se non ci sono mosse precedenti disattivo il bottone.
+    if(current_move == moves.cend()) {
+        ui->pushButton_previous->setEnabled(false);
+    }
+    //Abilito bottone next
+    ui->pushButton_next->setEnabled(true);
+}
+
+void MainWindow::on_pushButton_next_clicked()
+{
+    --current_move;
+    //Se non ci sono mosse successive disattivo il bottone.
+    if(current_move == moves.cbegin()) {
+        ui->pushButton_next->setEnabled(false);
+    }
+    //Abilito bottone previous
+    ui->pushButton_previous->setEnabled(true);
+
+    //Mostro la mossa successiva
+    QWidget *w = ui->gridLayout->itemAtPosition(
+                current_move->row,
+                current_move->column
+            )->widget();
+    QLineEdit* cell = static_cast<QLineEdit*>(w);
+    cell->setText(QString::number(current_move->value));
+}
+
+bool MainWindow::solve() {
+    int row, column;
+    if (!find_empty_cell(row, column)) {
+        // Tutte le celle sono state risolte
+        return true;
+    }
+
+    sudoku[row][column] = 1;
+    while (sudoku[row][column] <= 9) {
+        if (valid_cell(row, column) && solve()) {
+            moves.append(Move(row, column, sudoku[row][column]));
+            return true;
+        }
+        ++(sudoku[row][column]);
+    }
+
+    sudoku[row][column] = 0;
+    return false;
+}
+
+bool MainWindow::find_empty_cell(int& row, int& column) {
+    for (row = 0; row < 9; ++row) {
+        for (column = 0; column < 9; ++column) {
+            if (sudoku[row][column] == 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void MainWindow::save_sudoku(){
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            QWidget *w = ui->gridLayout->itemAtPosition(i, j)->widget();
+            QLineEdit* cell = static_cast<QLineEdit*>(w);
+            if (cell->text().length() > 0) {
+                sudoku[i][j] = cell->text().toShort();
+            }
+            else {
+                sudoku[i][j] = 0; //Cella non compilata
+            }
+        }
+    }
+}
+
+bool MainWindow::valid_cell(int row, int column) {
+    short number = sudoku[row][column];
+
+    //Controllo riga
+    for (int i = 0; i < 9; ++i) {
+        if (i != column && sudoku[row][i] == number) {
+            return false;
+        }
+    }
+
+    //Controllo colonna
+    for (int i = 0; i < 9; ++i) {
+        if (i != row && sudoku[i][column] == number) {
+            return false;
+        }
+    }
+
+    //Controllo casella 3x3
+    int box_row = row - (row % 3);
+    int box_column = column - (column % 3);
+    for (int i = box_row; i < box_row + 3; ++i) {
+        for (int j = box_column; j < box_column + 3; ++j) {
+            if ((i != row || j != column) && sudoku[i][j] == number) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void MainWindow::print_sudoku() {
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            QWidget *w = ui->gridLayout->itemAtPosition(i, j)->widget();
+            QLineEdit* cell = static_cast<QLineEdit*>(w);
+            if(cell->text().isEmpty()){
+                cell->setStyleSheet("color: red");
+                cell->setText(QString::number(sudoku[i][j]));
+            }
+        }
+    }
+}
---
+++ Qt/Sudoku/mainwindow.h
@@ -0,0 +1,83 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVector>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_resolve_clicked();
+
+    void on_pushButton_clear_clicked();
+
+    void on_pushButton_previous_clicked();
+
+    void on_pushButton_next_clicked();
+
+private:
+    short sudoku[9][9]; ///< Matrice contenente il Sudoku
+
+    /**
+     * @brief Struct contenente una mossa.
+     */
+    struct Move {
+        int row;
+        int column;
+        short value;
+
+        Move(): row(0), column(0), value(0) {}
+        Move(int r, int c, int v): row(r), column(c), value(v) {}
+    };
+    QVector<Move> moves; ///< Mosse effettuate dal solver
+    QVector<Move>::const_iterator current_move;
+
+    Ui::MainWindow *ui;
+
+    /**
+     * @brief Funzione che risolve il Sudoku in modo ricorsivo.
+     * @return true se il sudoku è stato risolto, false altrimenti.
+     */
+    bool solve();
+
+    /**
+     * @brief Salva i valori inseriti tramite UI nella variabile sudoku.
+     */
+    void save_sudoku();
+
+    /**
+     * @brief Funzione che controlla la validità di una cella nel Sudoku.
+     * @param row Riga della cella da validare
+     * @param column Colonna della cella da validare
+     * @return true se la cella rispetta le regole del Sudoku, false altrimenti.
+     */
+    bool valid_cell(const int& row, const int& column);
+
+    /**
+     * @brief Mostra il contenuto di Sudoku tramite UI.
+     * Mostra nell'interfaccia grafica i valori ottenuti dal solver con il
+     * colore rosso.
+     */
+    void print_sudoku();
+
+    /**
+     * @brief Trova una cella vuota nel sudoku.
+     * I parametri row e column vengono usati come output se viene trovata una
+     * cella vuota.
+     *
+     * @param row Riga della cella eventualmente trovata.
+     * @param column Colonna della cella eventualmente trovata
+     * @return true se è stata trovata una cella vuota, false altrimenti.
+     */
+    bool find_empty_cell(int& row, int& column);
+};
+#endif // MAINWINDOW_H
